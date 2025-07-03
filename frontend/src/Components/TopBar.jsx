2733b4d3a737c6caaa89c6a86a37bf330ed461d6
Harsh Kumar <harshmastic@gmail.com>
2025-07-03 12:39:39 +0530

    Add Serach Modal

--- frontend/src/Components/TopBar.jsx
+++ frontend/src/Components/TopBar.jsx
@@ -1,9 +1,10 @@
 import { RotateCcw } from "lucide-react";
-import { useEffect } from "react";
 import axios from "axios";
 import Switch2 from "./ui/Switch2";
+import { formatEmailTime } from "./utils/formatEmailTime";
 
-export function TopBar() {
+// Accept setEmails as a prop
+export function TopBar({ setEmails }) {
   const fetchEmailDetails = async () => {
     const token = localStorage.getItem("access_token");
 
@@ -31,6 +32,17 @@ export function TopBar() {
             },
           }
         );
+        const fromHeader = res.data.payload.headers.find(
+          (h) => h.name === "From"
+        );
+        let from = fromHeader?.value || "(No From)";
+        from = from.split("<")[0];
+
+        const dateHeader = res.data.payload.headers.find(
+          (h) => h.name === "Date"
+        );
+        let date = dateHeader?.value || "(No Date)";
+        date = formatEmailTime(date);
 
         const subjectHeader = res.data.payload.headers.find(
           (h) => h.name === "Subject"
@@ -40,6 +52,10 @@ export function TopBar() {
         return {
           id: msg.id,
           subject,
+          from,
+          time: date,
+          avatar: from.trim()[0].toUpperCase(),
+          avatarColor: "bg-gray-600",
         };
       })
     );
@@ -49,17 +65,13 @@ export function TopBar() {
 
   return (
     <div className="bg-[#1a1a1a] w-140 px-4 py-2 flex h-12 rounded-t-2xl">
-
       <div className="flex justify-between w-full space-x-4">
-      <div className="flex items-center  space-x-4">
-        <div className="flex items-center space-x-3">
-        
-      </div>
+        <div className="flex items-center  space-x-4">
+          <div className="flex items-center space-x-3"></div>
           <div className="w-4 h-4 border border-gray-500 rounded"></div>
           <div className="w-4 h-4 border border-gray-500 rounded"></div>
         </div>
         <div className="flex items-center space-x-3">
-          {/* <div className="w-2 h-2 bg-green-500 rounded-full"></div> */}
           <Switch2 />
           <span className="text-sm text-[#cccccc]">Auto label</span>
           <button
